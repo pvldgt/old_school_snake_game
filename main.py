af8426c40bc37ab8d401da2b76127323c17e2631
Pavel Degtyarev <pvldgt@gmail.com>
2022-04-17 00:33:03 +0300

    create snake class and move snake related code there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 # set up the screen background
 screen = Screen()
@@ -11,19 +12,7 @@ screen.title("Old School Snake Game")
 # instead it will move smoothly
 screen.tracer(0)
 
-# set up the starting positions
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-# a for loop to create the 3 initial segments
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 # a while loop to make the initial segments move along
 game_is_on = True
@@ -32,27 +21,8 @@ while game_is_on:
     screen.update()
     # adding a 0.1 sec delay between each screen update to imitate the snake moving
     time.sleep(0.1)
-    # create a for loo[ that moves the snake segments, last to second to last etc
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    # and then move the very first element that will drag the rest of the elements
-    # on the next iteration
-    segments[0].forward(20)
-
-
-# snake_segment_1 = Turtle(shape="square")
-# snake_segment_1.color("white")
-# snake_segment_1.goto(0, 0)
-#
-# snake_segment_2 = Turtle(shape="square")
-# snake_segment_2.color("white")
-# snake_segment_2.goto(-20, 0)
-#
-# snake_segment_3 = Turtle(shape="square")
-# snake_segment_3.color("white")
-# snake_segment_3.goto(-40, 0)
+    # function that moves the snake segments, last to second to last etc
+    snake.move()
 
 
 
---
+++ snake.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+# set up the starting positions
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        # a for loop to create the 3 initial segments
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # create coordinates of the second to last segment
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        # and then move the very first element that will drag the rest of the elements
+        # on the next iteration
+        self.segments[0].forward(MOVE_DISTANCE)
