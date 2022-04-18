9ac98d456c7c18e1d709cd8e4dc520b2dafdf5d1
Pavel Degtyarev <pvldgt@gmail.com>
2022-04-18 18:13:52 +0300

    fix wall collision, on collision snake appears from the other side of the screen

--- main.py
+++ main.py
@@ -48,10 +48,15 @@ while game_is_on:
         snake.extend()
         scoreboard.refresh_counter()
 
-    # detect collision with wall
-    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        game_is_on = False
-        scoreboard.game_over()
+    # detect collision with wall, if snake head goes over the limit then appear from the other side of the screen
+    if snake.head.xcor() > 290:
+        snake.head.goto(-290, snake.head.ycor())
+    elif snake.head.xcor() < -290:
+        snake.head.goto(290, snake.head.ycor())
+    elif snake.head.ycor() > 290:
+        snake.head.goto(snake.head.xcor(), -290)
+    elif snake.head.ycor() < -290:
+        snake.head.goto(snake.head.xcor(), 290)
 
     # detect collision with tail
     # if head collides with any segment of the snake then trigger game over sequence
