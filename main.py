882b3d631c820a2bfced384524730f73ed7e088e
Pavel Degtyarev <pvldgt@gmail.com>
2022-04-18 15:13:12 +0300

    add wall collision check and create scoreboard.game_over method

--- main.py
+++ main.py
@@ -36,9 +36,15 @@ while game_is_on:
     # function that moves the snake segments, last to second to last etc
     snake.move()
 
+    # detect collision with food
     if snake.head.distance(food) < 18:
         food.refresh()
         scoreboard.refresh_counter()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 
